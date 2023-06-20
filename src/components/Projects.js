354573ddc0391fb87e946a4350934c792196a2ce
Anshi Paul <anshipaul@outlook.com>
2023-06-20 14:00:39 -0400

    Initial commit

---
+++ src/components/Projects.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { projects } from "../data";
+
+export default function Projects() {
+    return (
+        <section id="projects" className="text-gray-400 bg-gray-900 body-font">
+          <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+            <div className="flex flex-col w-full mb-20">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-10 h-10 mx-auto mb-4 animate-pulse">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
+                </svg>
+              <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
+                Projects
+              </h1>
+              <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+                View past projects entered into science fairs such as the Regeneron International Science and Engineering Fair, Pennsylvania Junior Academy of Science, Capital Area Science and Engineering Fair, and International BioGENEius Challenge.
+              </p>
+            </div>
+            <div className="flex flex-wrap -m-4">
+              {projects.map((project) => (
+                <a
+                  href={project.link}
+                  key={project.image}
+                  className="sm:w-1/2 w-100 p-4">
+                  <div className="flex relative">
+                    <img
+                      alt="gallery"
+                      className="absolute inset-0 w-full h-full object-cover object-center rounded-2xl"
+                      src={project.image}
+                    />
+                    <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 rounded-2xl opacity-0 hover:opacity-100">
+                      <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                        {project.subtitle}
+                      </h2>
+                      <h1 className="title-font text-lg font-medium text-white mb-3">
+                        {project.title}
+                      </h1>
+                      <p className="leading-relaxed">{project.description}</p>
+                    </div>
+                  </div>
+                </a>
+              ))}
+            </div>
+          </div>
+        </section>
+      );
+}
---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const projects = [
+  {
+    title: "NEMO: Wearable Memory Assistive Device for Dementia and Alzheimer’s Patients Using Machine Learning Facial Recognition",
+    subtitle: "Facial Recognition, Python, RaspberryPi, Flutter",
+    description:
+      "2022-23 | Wearable device aiming to increase patient social confidence through auditory reminders of names of faces.",
+    image: "./nemo_image.png",
+    link: "https://projectboard.world/isef/project/enbm084-wearable-facial-recognition-device-for-dementia",
+  },
+  {
+    title: "BraillEZ: Bridging the Deafblind Communication Gap",
+    subtitle: "Python, RaspberryPi",
+    description:
+      "2020-22 | Tabletop communication device with a refreshable braille display and keyboard for portable and quick conversational braille/text translation.",
+    image: "./braillez_logo.png",
+    link: "",
+  },
+  {
+    title: "RemindMEd: A Device to Remind When You Forget",
+    subtitle: "Arduino",
+    description:
+      "2019-20 | Smart reminding pillbox to help dementia patients adhere to medicine regimen.",
+    image: "./remindmed_logo.png",
+    link: "",
+  },
+];
