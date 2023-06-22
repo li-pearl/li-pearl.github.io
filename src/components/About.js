f27cc2bb8d337685bdd19f0a43014088f62f70c9
Anshi Paul <anshipaul@outlook.com>
2023-06-22 11:26:05 -0400

    minor changes

--- src/components/About.js
+++ src/components/About.js
@@ -7,16 +7,16 @@ export default function About() {
             <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
                 <div className="lg:flex-grow md:w-1/3 lg:pr-24 md:pr-16 flex flex-col md:text-left mb-16 md:mb-0 items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-                        Hello! I'm Anshi Paul
+                        Hi! I'm Anshi Paul
                         <br className="hidden lg:inline-block"/>
                         <Typed
                             className="text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-green-600"
-                            style={{ margin: "o o 1rem o" }}
+                            // style={{ margin: "o o 1rem o" }}
                             typeSpeed={100}
                             backSpeed={50}
                             backDelay={1000}
                             loop
-                            strings={["Programmer", "Maker", "Innovator"]}
+                            strings={["Programmer", "Maker", "Innovator", "FRC Competitor"]}
                         />
                     </h1>
                     <p className="mb-8 leading-relaxed">
