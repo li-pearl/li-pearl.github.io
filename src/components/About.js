9bb0ef8de068f844c2a525b0c091ed880b99b50e
Anshi Paul <anshipaul@outlook.com>
2024-01-27 20:05:38 -0500

    Updated About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,17 @@
 import React from "react";
 import Typed from "react-typed";
+// import Spline from "@splinetool/react-spline";
 
 export default function About() {
   return (
     <section id="about">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/3 lg:pr-24 md:pr-16 flex flex-col md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+        <div className="lg:flex-grow md:w-1/3 lg:pr-24 lg:pl-20 md:pr-16 flex flex-col mb-16 md:mb-0 items-left">
+          <h1 className="title-font sm:text-6xl text-6xl mb-4 font-medium text-white">
             Hi! I'm Anshi Paul
             <br className="hidden lg:inline-block" />
           </h1>
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium">
+          <h1 className="title-font sm:text-5xl text-5xl mb-4 font-medium">
             <Typed
                 className="text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-green-600"
                 // style={{ margin: "o o 1rem o" }}
@@ -18,14 +19,14 @@ export default function About() {
                 backSpeed={50}
                 backDelay={1000}
                 loop
-                strings={["Programmer", "Maker", "Innovator", "FRC Competitor"]}
+                strings={["Programmer", "Maker", "Innovator", "FRC Competitor", "Aviation Enthusiast"]}
               />
           </h1>
           <p className="mb-8 leading-relaxed">
-            Developing assistive technology and devices to make the world a
+            A high school junior developing assistive technology and devices to make the world a
             better place.
           </p>
-          <div className="flex justify-center">
+          <div className="flex justify-left">
             <a
               href="#projects"
               className="inline=flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
@@ -41,6 +42,7 @@ export default function About() {
           </div>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          {/* <Spline scene="https://prod.spline.design/oaGIYQYrLKGnbG2w/scene.splinecode" /> */}
           <img
             className="object-cover object-center rounded"
             alt="plane"
