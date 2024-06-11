e7a7ebc38bd392e27faff24e73b1540fba8134e6
Anshi Paul <anshipaul@outlook.com>
2024-06-11 11:53:17 -0400

    help

---
+++ build/precache-manifest.ab322e3768644247b3b1c572424d9f25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bbfacc79cbb5d8a3fac8",
+    "url": "/static/css/main.608f6220.chunk.css"
+  },
+  {
+    "revision": "bbfacc79cbb5d8a3fac8",
+    "url": "/static/js/main.bbfacc79.chunk.js"
+  },
+  {
+    "revision": "d1952b8f0a618de8bf76",
+    "url": "/static/js/1.d1952b8f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ced81a5836d3057d966ac533b132398d",
+    "url": "/index.html"
+  }
+];
